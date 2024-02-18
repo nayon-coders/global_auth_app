d3f799cbb2d411825e503e0db2ffff8ebbd2154a
Nayon Talukder <69708059+nayon-coders@users.noreply.github.com>
2024-02-18 21:57:05 +0600

    main

--- android/app/src/main/kotlin/com/alloneautos/driver/MainActivity.kt
+++ android/app/src/main/kotlin/com/alloneautos/driver/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.driver
+package com.alloneautos.driver
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,42 @@
+import 'package:driver/view/bottom_navigation/screen/bootom_navigation.dart';
 import 'package:driver/view/flash/flash.dart';
+import 'package:driver/widgets/app_toast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+
+  _handleLocationPermission();
   runApp(const MyApp());
+}
+
+Future<bool> _handleLocationPermission() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    AppToast(text: 'Location services are disabled. Please enable the services', color: Colors.red);
+    return false;
+  }
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      AppToast(text: 'Location permissions are denied', color: Colors.red);
+      return false;
+    }
+  }
+  if (permission == LocationPermission.deniedForever) {
+    AppToast(text: 'Location permissions are permanently denied, we cannot request permissions.', color: Colors.red);
+    return false;
+  }
+  return true;
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +52,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         fontFamily: "Poppins"
       ),
-      home: Flash(),
+      home:  FirebaseAuth.instance.currentUser == null ? AppBottomNavigation() : Flash(),
     );
   }
 }
